da0dfef33399281da05282088e45495791218fdf
HackNovas <creatives.doni@gmail.com>
2022-07-05 13:47:24 +0530

    Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,31 +3,12 @@ import PropTypes from 'prop-types';
 
 
 export default function Navbar(props) {
-    let togleMode=()=>{
-        
-    }
 
     return (
         <>
             <nav className={`navbar navbar-expand-lg navbar-light bg-light`}>
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">{props.title}</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">{props.about}</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" onClick={togleMode} role="switch" id="flexSwitchCheckDefault" />
+                <div className="container-fluid" >
+                    <a className="navbar-brand " href="/">{props.title}</a>
                 </div>
             </nav>
         </>
@@ -37,9 +18,4 @@ export default function Navbar(props) {
 Navbar.propTypes = {
     title: PropTypes.string,
     about: PropTypes.string
-};
-
-Navbar.defaultProps = {
-    title: "Enter Titles",
-    about: "Write about us"
 };
